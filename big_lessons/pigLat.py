a4c05bcdd7dac8c95f3dc6f8f8ab86c64560ace2
Mikhail Rusin <Mikhail.Rusin01@gmail.com>
2022-08-01 21:59:36 +0700

    missed

--- big_lessons/pigLat.py
+++ big_lessons/pigLat.py
@@ -12,7 +12,7 @@ for word in message.split():
     while len(word) > 0 and not word[0].isalpha:
         prefixNonLetters += word[0]
         word = word[1:]
-    if len[word] == 0:
+    if len(word) == 0:
         pigLat.append[prefixNonLetters]
         continue
     # Определяем "небуквы" в конце слова
@@ -20,4 +20,29 @@ for word in message.split():
     while not word[-1].isalpha():
         suffixNonLetters += word[-1]
         word = word[:-1]
+    # Запоминаем регистр слов
+    wasUpper = word.isupper()
+    wasTitel = word.istitle()
+    word = word.lower() # перевод в нижний регистр
+    # Отделяем согласные в начале слова
+    prefixConstants = ''
+    while len(word) > 0 and not word[0] in VOWELS:
+        prefixConstants += word[0]
+        word = word[1:]
+    # Присоединяем в правильной последовательности
+    if prefixConstants != '':
+        word += prefixConstants + 'ay'
+    else:
+        word += 'yay'
+    # Возвращаем исходный регистр
+    if wasUpper:
+        word = word.upper()
+    if wasTitel:
+        word = word.title()
+    # Возвращаем небуквенные символы в начало или конец слова
+    pigLat.append(prefixNonLetters + word + suffixNonLetters)
+# Соединяем слова обратно в строку
+print(' '.join(pigLat))
+
+    
 
