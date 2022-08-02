c07c2a8c4ffd3871f37ae5db04f9773f4fd38f8d
Mikhail Rusin <Mikhail.Rusin01@gmail.com>
2022-08-02 21:43:19 +0700

    HW4_2

--- homework_4/table.py
+++ homework_4/table.py
@@ -8,12 +8,12 @@ def printTable(data):
         for item in list:
             if len(item) > colWidths[data.index(list)]:
                 colWidths[data.index(list)] = len(item)
-        for item in list:
-            print (item.rjust(colWidths[data.index(list)], '.'))
+        #for item in list:
+            #print (item.rjust(colWidths[data.index(list)], '.'))
         # Как напечатать правильно?!?!?!?
-    for i in range(len(tableData)):
-        for j in range(len(i)):
-            print()
+    for i in range(len(tableData[0])):
+        print(str(data[0][i]).rjust(colWidths[0]) + ' ' + str(data[1][i]).rjust(colWidths[1]) + \
+        ' ' + str(data[2][i]).rjust(colWidths[2]))
 
 printTable(tableData)
 
